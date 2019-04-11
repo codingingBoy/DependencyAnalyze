3ac95a08df0556158f8b5029fc88c655d88028bb
jgl <jiangguilin@xunlei.com>
2019-04-11 09:40:16 +0800

    init project and readme

---
+++ DependencyAnalyzer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DependencyAnalyzer
+//
+//  Created by JGL on 2019/4/11.
+//  Copyright © 2019 JGL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DependencyAnalyzer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DependencyAnalyzer
+//
+//  Created by JGL on 2019/4/11.
+//  Copyright © 2019 JGL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
